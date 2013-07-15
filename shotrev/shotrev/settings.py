aafa47e8bd2ee671fd5638d82f4c1ec99e0df5d9
Kalail <kashif610@gmail.com>
2013-07-15 16:37:47 -0500

    Recommit

--- shotrev/shotrev/settings.py
+++ shotrev/shotrev/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_DIR.child('static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     'south',
     'suit',
     'django.contrib.admin',
+    'gunicorn',
 
     # App
     'landing',
